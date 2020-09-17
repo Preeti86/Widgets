9febc8bc14c4bc815ef6bab073d82d2359e23ae7
Preeti <preetijav@gmail.com>
2020-09-17 13:21:19 +0200

    Added accordion widget

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javaScript frameworkd'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among enginners'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'you use React by creating components'
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion items={items}/>
     </div>
   );
 }
